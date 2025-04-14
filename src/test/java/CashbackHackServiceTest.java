136277c2111701c4aa178598f4dc3e106eb119c3
ksu <ksunoonnn@yandex.ru>
2025-04-14 16:00:12 +0300

    JUnit4 tests

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,5 +1,5 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
@@ -8,9 +8,10 @@ public class CashbackHackServiceTest {
 
     @Test
     public void testRemain() {
-        Assert.assertEquals(service.remain(900), 100);
-        Assert.assertEquals(service.remain(1000), 0);
-        Assert.assertEquals(service.remain(1500), 500);
+        Assert.assertEquals(100, service.remain(900));
+        Assert.assertEquals(0, service.remain(1000));
+        Assert.assertEquals(500, service.remain(1500));
     }
 }
+
 
