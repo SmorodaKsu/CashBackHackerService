d48e21bfb6caf976adb27970e945311921b08a24
ksu <ksunoonnn@yandex.ru>
2025-04-14 15:58:53 +0300

    TestNG tests

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,2 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
 public class CashbackHackServiceTest {
+
+    private final CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testRemain() {
+        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(1500), 500);
+    }
 }
+
